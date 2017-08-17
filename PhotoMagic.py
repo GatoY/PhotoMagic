f72925afec3abe30f44ee668a7f6b01baeed446e
GatoL <17721327159@163.com>
2017-08-17 10:03:42 +1000

    Add files via upload

---
+++ PhotoMagic.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+def resize (img,height=None,width=None): #change the height, width of pics
+    if height==None and width==None:
+        return img
+    #if inputs are none
+    if height==None: #width!=None
+        img=cv2.resize(img,(width,img.shape[0]),interpolation=cv2.INTER_AREA)
+    if width==None:  #height!=None
+        img=cv2.resize(img,(img.shape[1],height),interpolation=cv2.INTER_AREA)
+    else: #width!=None,height!=None
+        img=cv2.resize(img,(width,height),interpolation=cv2.INTER_AREA)
+    return img
+
+def gray(img): #change pics to gray mode
+    return cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+def colorchange(img,r=0,g=0,b=0):
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if r<0:
+                if img.item(i,j,0)+r>0:img.itemset((i,j,0),img.item(i,j,0)+r)
+                else:img.itemset((i,j,0),0)
+                print 1
+            #rgb cannot lower than 0.
+            else:
+                if img.item(i,j,0)+r<255:img.itemset((i,j,0),img.item(i,j,0)+r)
+                else:img.itemset((i,j,0),255)
+                print 2
+            #rgb cannot higher than 255.
+            if g<0:
+                if img.item(i,j,1)+g>0:img.itemset((i,j,1),img.item(i,j,1)+g)
+                else:img.itemset((i,j,1),0)
+                print 3
+            else:
+                if img.item(i,j,1)+g<255:img.itemset((i,j,1),img.item(i,j,1)+g)
+                else:img.itemset((i,j,1),255)
+                print 4
+            if b<0:
+                if img.item(i,j,2)+b>0:img.itemset((i,j,2),img.item(i,j,2)+b)
+                else:img.itemset((i,j,2),0)
+                print 5
+            else:
+                if img.item(i,j,2)+b<255:img.itemset((i,j,2),img.item(i,j,2)+b)
+                else:img.itemset((i,j,2),255)
+                print 6
+    return img
+
+def roi(img,x1,y1,x2,y2):
+    #x1,y1 is the coordinates of mouse's first clicking
+    #x2,y2 is of the second
+    roi = img[y1:y2,x1:x2]
+    return roi
